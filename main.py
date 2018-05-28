0f1ed49e77180d90f7b3303ec9062fbeec9947e5
Madis-Karli Koppel <matukoppel@gmail.com>
2018-05-28 19:39:28 +0300

    refactor car detection
    create main file that reads video and finds cars from them

--- main.py
+++ main.py
@@ -1,22 +1,65 @@
-import numpy as np
+import argparse
+import os
 import cv2
+import shutil
+from yolonet import find_cars
 
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print "ERROR"
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+# read a video file and detect all cars from it
+def detect_cars_from_video(video_loc, frames_to_skip, threshold):
 
-    # Our operations on the frame come here
-    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# Start yolonet
+	net, meta = find_cars.init_net()
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	# what frame are we currently erading
+	nr = 0
+	skip = frames_to_skip
+	frames_loc = 'videoframes'
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+	if not os.path.exists(frames_loc):
+		os.makedirs(frames_loc)
+
+	# Read input video using cv2
+	cap = cv2.VideoCapture(video_loc)
+
+	while cap.isOpened():
+		# check if open axxually, happens when the skip rate is too high
+		if not cap.isOpened():
+			break
+
+		ret, frame = cap.read()
+
+		# Stop if the video is over
+		if not ret:
+			break
+
+		nr += 1
+
+		if nr % skip != 0:
+			continue
+
+		detected_cars = find_cars.detect_cars_from_frame(frame, nr, threshold, net, meta)
+
+		print "Found {} car(s) from frame {}".format(str(len(detected_cars)), str(nr))
+
+	cap.release()
+	cv2.destroyAllWindows()
+
+	shutil.rmtree(frames_loc, ignore_errors=False)
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument("-v", "--video", help="path to input video", required=True)
+
+	parser.add_argument("-s", "--skip", help="number of frames to skip", default=15, type=int)
+
+	parser.add_argument("-t", "--threshold", help="threshold for darknet", default=0.5, type=float)
+
+	args = parser.parse_args()
+	print 'args'
+	print args
+
+	detect_cars_from_video(args.video, args.skip, args.threshold)
+
